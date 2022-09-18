68baa03de81ce3eb510360a639a1dec08e48acef
Akshita-Saini <60578602+Akshita-Saini@users.noreply.github.com>
2022-09-19 02:40:57 +0530

    Time: 21 ms (17.81%), Space: 45 MB (39.69%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -2,12 +2,8 @@ class Solution {
     public boolean isPalindrome(int x) {
         String digitsString = String.valueOf(x);
         int n = digitsString.length();
-        char digits[] = new char[n];
         for(int i=0;i<n;i++){
-            digits[i] = digitsString.charAt(i);   
-        }
-        for(int i=0;i<n/2;i++){
-            if(digits[i]!=digits[n-i-1]) return false;
+            if(digitsString.charAt(i)!=digitsString.charAt(n-1-i)) return false;  
         }
         return true;
     }
